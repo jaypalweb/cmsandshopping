9bcf5c09382d48c93ae51b4318e5a9c30f121619
Jay Pal <contactjaypal@gmail.com>
2020-03-13 14:43:17 +0530

    get add page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-//routes
+/**
+ * GET pages index
+ */
 router.get('/', function (req, res) {
-    res.send('admin area here');
+    res.render('admin/pages', { title: 'Admin Title' });
+});
+
+/**
+ * GET add page
+ */
+router.get('/add-page', function (req, res) {
+    var title = '';
+    var slug = '';
+    var content = '';
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    });
+
 });
 
 // Exports
