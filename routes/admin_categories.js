87d90ba678eee80ee50ab73c819ebea1d6810b0e
Jay Pal <contactjaypal@gmail.com>
2020-03-27 20:00:33 +0530

    edit category

--- routes/admin_categories.js
+++ routes/admin_categories.js
@@ -76,107 +76,72 @@ router.post('/add-category', [
 
     });
 
-
-/*
- * POST reorder pages
- */
-router.post('/reorder-pages', function (req, res) {
-    var ids = req.body['id[]'];
-    var count = 0;
-    for (var i = 0; i < ids.length; i++) {
-        var id = ids[i];
-        count++;
-
-        //the function closure is need as Db functions are async, so if not user
-        //count value will be last value for all
-        (function (count) {
-
-            Page.findById(id, function (err, page) {
-                page.sorting = count;
-                page.save(function (err) {
-                    if (err)
-                        return console.log(err);
-                })
-            });
-        })(count);
-    }
-
-});
-
 /**
- * GET edit page
+ * GET edit category
  */
-router.get('/edit-page/:slug', function (req, res) {
-    Page.findOne({ slug: req.params.slug }, function (err, page) {
+router.get('/edit-category/:id', function (req, res) {
+    Category.findById(req.params.id, function (err, category) {
         if (err)
             return console.log(err);
 
         //console.log(page);
-        res.render('admin/edit_page', {
-            title: page.title,
-            slug: page.slug,
-            content: page.content,
-            id: page._id
+        res.render('admin/edit_category', {
+            title: category.title,
+            id: category._id
         });
     });
 });
 
 
 /**
- * POST edit page
+ * POST edit category
  */
-router.post('/edit-page/:slug', [
+router.post('/edit-category/:id', [
     // username must be an email
-    check('title').not().isEmpty().withMessage('Title must have a value.'),
-    // password must be at least 5 chars long
-    check('content').not().isEmpty().withMessage('Content must have a value.')
+    check('title').not().isEmpty().withMessage('Title must have a value.')
 ],
     function (req, res) {
 
         var title = req.body.title;
-        var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-        if (slug == "")
-            slug = title.replace(/\s+/g, '-').toLowerCase();
-        var content = req.body.content;
-        var id = req.body.id;
-
+        var slug = title.replace(/\s+/g, '-').toLowerCase();
+        var id = req.params.id;
+        // console.log(id);
+        // process.exit();
         // Finds the validation errors in this request and wraps them in an object with handy functions
         const errors = validationResult(req);
         //console.log('errors', errors.array());
         if (!errors.isEmpty()) {
-            res.render('admin/edit_page', {
+            res.render('admin/edit_category', {
                 errors: errors.array(),
                 title: title,
-                slug: slug,
-                content: content,
                 id: id
             });
         } else {
-            Page.findOne({ slug: slug, _id: { '$ne': id } }, function (err, page) {
-                if (page) {
-                    req.flash('danger', 'Page slug exists, choose another.');
-                    res.render('admin/add_page', {
+            Category.findOne({ slug: slug, _id: { '$ne': id } }, function (err, category) {
+                if (category) {
+                    req.flash('danger', 'Category title exists, choose another.');
+                    res.render('admin/add_category', {
                         title: title,
-                        slug: slug,
-                        content: content,
                         id: id
                     });
                 } else {
-                    Page.findById(id, function (err, page) {
+                    Category.findById(id, function (err, category) {
+                        // console.log(category);
+                        // console.log(title);
+                        // process.exit();
                         if (err)
                             return console.log(err);
 
-                        page.title = title;
-                        page.slug = slug;
-                        page.content = content;
+                        category.title = title;
+                        category.slug = slug;
 
-                        page.save(function (err) {
+                        category.save(function (err) {
                             if (err)
                                 return console.log(err);
 
 
-                            req.flash('success', 'Page updated!');
-                            res.redirect('/admin/pages/edit-page/' + page.slug);
+                            req.flash('success', 'Category updated!');
+                            res.redirect('/admin/categories/edit-category/' + id);
                         });
                     });
 
