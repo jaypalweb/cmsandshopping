4eafc34a0ac46b1fb146a550cd5d162a6a56f8b3
Jay Pal <contactjaypal@gmail.com>
2020-03-29 01:06:33 +0530

    get add product

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -30,18 +30,20 @@ router.get('/', function (req, res) {
 });
 
 /**
- * GET add page
+ * GET add product
  */
-router.get('/add-page', function (req, res) {
+router.get('/add-product', function (req, res) {
     var title = '';
-    var slug = '';
-    var content = '';
-    res.render('admin/add_page', {
-        title: title,
-        slug: slug,
-        content: content
+    var desc = '';
+    var price = '';
+    Category.find(function (err, categories) {
+        res.render('admin/add_product', {
+            title: title,
+            desc: desc,
+            categories: categories,
+            price: price
+        });
     });
-
 });
 
 /**
