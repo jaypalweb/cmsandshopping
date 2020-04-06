98085685c554fe06bdb1395d7021401fa642e42f
Jay Pal <contactjaypal@gmail.com>
2020-04-06 18:24:06 +0530

    display page content

--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-//routes
+//Get page model
+var Page = require('../models/page');
+
+/**
+ * GET /
+ */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Home' });
+    Page.findOne({ slug: 'home' }, function (err, page) {
+        if (err)
+            return console.log(err);
+
+        res.render('index',
+            {
+                title: page.title,
+                content: page.content
+            }
+        );
+    });
 });
+
+/**
+ * GET a page
+ */
+router.get('/:slug', function (req, res) {
+    var slug = req.params.slug;
+    Page.findOne({ slug: slug }, function (err, page) {
+        if (err)
+            return console.log(err);
+
+        if (!page) {
+            res.redirect('/');
+        } else {
+            res.render('index',
+                {
+                    title: page.title,
+                    content: page.content
+                }
+            );
+        }
+    });
+
+});
+
 
 // Exports
 module.exports = router;
